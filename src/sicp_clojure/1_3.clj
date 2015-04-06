bf5eac1e5646dd1717733eec0d54c71b62d4f891
toby <tobytronics@gmail.com>
2015-04-06 18:18:59 +0100

    Ex 1.3

---
+++ src/sicp_clojure/1_3.clj
@@ -0,0 +1,8 @@
+(defn square [x]
+        (* x x))
+
+(defn sum-of-squares [a b]
+        (+ (square a) (square b)))
+
+(defn sum-of-squares-of-largest-2 [a b c]
+  (apply sum-of-squares (take 2 (reverse (sort [a b c])))))
