e382f153a890ad835385e6266c1b4482da5365aa
toby <tobytronics@gmail.com>
2015-04-06 20:30:30 +0100

    Fix comments

--- src/sicp_clojure/exercises/1_2.clj
+++ src/sicp_clojure/exercises/1_2.clj
@@ -1,5 +1,8 @@
 ;; Exercise 1.2.
 ;; Translate the following expression into prefix form
+;; 5 + 4 + (2 - (3 - (6 + 4/5)))
+;; -----------------------------
+;;        3(6 - 2)(2 - 7)
 
 (ns sicp-clojure.exercises.1-2
   (:require [clojure.test :refer :all]))
