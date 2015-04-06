767b72c8a1da07667097868ccd9f130f04d1ca19
toby <tobytronics@gmail.com>
2015-04-06 18:23:24 +0100

    Update to 1.3 having looked around the web for improvements

--- src/sicp_clojure/1_3.clj
+++ src/sicp_clojure/1_3.clj
@@ -1,8 +1,8 @@
-(defn square [x]
+(defn- square [x]
         (* x x))
 
-(defn sum-of-squares [a b]
+(defn- sum-of-squares [a b]
         (+ (square a) (square b)))
 
 (defn sum-of-squares-of-largest-2 [a b c]
-  (apply sum-of-squares (take 2 (reverse (sort [a b c])))))
+  (apply sum-of-squares (take 2 (sort > [a b c]))))
