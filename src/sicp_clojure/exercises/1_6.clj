6a44a4ad5e1c160b82a3a2de9b9dce150e89d231
toby <tobytronics@gmail.com>
2015-05-04 13:45:53 +0100

    Ex 1.9

--- src/sicp_clojure/exercises/1_6.clj
+++ src/sicp_clojure/exercises/1_6.clj
@@ -1,4 +1,5 @@
-;; Exercise 1.6: Alyssa P. Hacker doesn’t see why if needs to be pro-
+;; Exercise 1.6:
+;; Alyssa P. Hacker doesn’t see why if needs to be pro-
 ;; vided as a special form. “Why can’t I just define it as an ordinary
 ;; procedure in terms of cond ?” she asks. Alyssa’s friend Eva Lu Ator
 ;; claims this can indeed be done, and she defines a new version of
